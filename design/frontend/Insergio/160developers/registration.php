08154fbed6c6de1713e1d7a7735a1170320860f8
insergio <sergiodelahoz26@gmail.com>
2019-03-03 01:18:20 -0500

    inicial

---
+++ design/frontend/Insergio/160developers/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Insergio/160developers',
+    __DIR__
+);
